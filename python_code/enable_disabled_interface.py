113ef0227ca2b20fad42248889fc6b7d6ff3c79a
Davala Mohan Raj <mohanonline.93@gmail.com>
2020-06-23 11:57:31 +0800

    Create enable_disabled_interface.py

---
+++ python_code/enable_disabled_interface.py
@@ -0,0 +1,30 @@
+from jinja2 import Template
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+import requests
+from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+def get_junos_details(dev):
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+	healthbot_user = 'jcluser'
+	healthbot_pwd = 'Juniper!1'
+	healthbot_server = '100.123.35.0'
+	headers = { 'Accept' : 'application/json', 'Content-Type' : 'application/json' }
+	url = 'https://'+ healthbot_server + ':8080/api/v1/device/' + dev +'/'
+	r = requests.get(url, auth=HTTPBasicAuth(healthbot_user, healthbot_pwd), headers=headers, verify=False)
+	return r.json()
+
+def enable_interface(int, **kwargs):
+	junos_details = get_junos_details(kwargs['device_id'])
+	junos_host = junos_details['host']
+	junos_user = junos_details['authentication']['password']['username']
+	# junos_password = junos_details['authentication']['password']['password']
+	junos_password = 'Juniper!1'
+	device=Device(host=junos_host, user=junos_user, password=junos_password)
+	device.open()
+	cfg=Config(device)
+	my_template = Template('delete interfaces {{ interface }} disable')
+	cfg.load(my_template.render(interface = int), format='set')
+	cfg.commit()
+	device.close()
