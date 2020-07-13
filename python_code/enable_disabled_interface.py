c2cd8266b934a0f026b67db8e21f34b685bf210b
Davala Mohan Raj <mohanonline.93@gmail.com>
2020-07-13 16:28:31 +0800

    Update enable_disabled_interface.py

--- python_code/enable_disabled_interface.py
+++ python_code/enable_disabled_interface.py
@@ -7,9 +7,10 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 def get_junos_details(dev):
 	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-	healthbot_user = 'jcluser'
-	healthbot_pwd = 'Juniper!1'
-	healthbot_server = '100.123.35.0'
+	#healthbot user, pwd and server details are not mandatory to make a api server call
+	#healthbot_user = 'jcluser'
+	#healthbot_pwd = 'Juniper!1'
+	#healthbot_server = '100.123.35.0'
 	headers = { 'Accept' : 'application/json', 'Content-Type' : 'application/json' }
 	url = 'https://api-server:9000/api/v1/device/' + dev +'/'
 	r = requests.get(url, headers=headers, verify=False)
